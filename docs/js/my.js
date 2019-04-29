38370ec552875071a9b0d7d814a800436830b738
Mark Veltzer <mark.veltzer@gmail.com>
2019-04-29 05:02:41 +0300

    with images

--- docs/js/my.js
+++ docs/js/my.js
@@ -2,23 +2,32 @@ $(document).ready(function() {
 	$('#tabs').tabslet({
 		animation: true,
 	});
+	html_loading="<div id='msg'>Loading data...</div>";
 	dataContainer=$('#books-data');
-	$('#pagination-container').pagination({
-		dataSource: "books-data.json",
-		locator: 'items',
-		totalNumberLocator: function(response) {
-			return response.items.length;
-		},
-		pageSize: 4,
-		ajax: {
-			beforeSend: function() {
-				dataContainer.html('Loading data...');
-			}
-		},
-		callback: function(data, pagination) {
-			// template method of yourself
-			//var html = template(data);
-			dataContainer.html('name is '+data[0].name);
-		}
+	dataContainer.html(html_loading);
+	function simpleTemplating(data) {
+		var html ='';
+		$.each(data, function(index, item){
+			html += '<div class="card">';
+			html += '<img src="img_avatar.png" alt="Avatar" style="width:100%">';
+			html += '<div class="container">';
+			html += '<h4><b>'+ item.name +'</b></h4>';
+			html += '<p>'+item.author +'</p';
+			html += '</div>';
+			html += '</div>';
+		});
+		return html;
+	}
+	$.getJSON('books-data.json', function(data) {
+		$('#pagination-container').pagination({
+			dataSource: data.items,
+			totalNumberLocator: function(response) {
+				return data.items.length;
+			},
+			pageSize: 4,
+			callback: function(data, pagination) {
+				dataContainer.html(simpleTemplating(data));
+			},
+		});
 	});
 });
