9470d9b1b4f58ae57a74ab9f8ce43c328c019e5f
Mark Veltzer <mark.veltzer@gmail.com>
2019-04-29 06:31:00 +0300

    first book actually shown

--- docs/js/my.js
+++ docs/js/my.js
@@ -10,10 +10,21 @@ $(document).ready(function() {
 		$.each(data, function(index, item){
 			html += '<div class="column">';
 			html += '<div class="card">';
-			html += '<img src="img_avatar.png" alt="Avatar" style="width:100%"></img>';
+			html += '<img src="'+item.image+'" style="width:100%"></img>';
 			html += '<div class="card_container">';
-			html += '<h4><b>'+ item.name +'</b></h4>';
-			html += '<p>'+item.author +'</p>';
+			html += '<h3>'+ item.title+'</h3>';
+			html += '<h4>'+ item.subtitle+'</h4>';
+			html += '<ul class="bc-list-nostyle">';
+			html += '<li>By: '+item.authors+'</li>';
+			html += '<li>Narrated by: '+item.narrators+'</li>';
+			html += '<li>Length: '+item.length+'</li>';
+			html += '<li>'+item.type+'</li>';
+			html += '<li>Release Date: '+item.released+'</li>';
+			html += '<li>Language: '+item.language+'</li>';
+			html += '<li>Publisher: '+item.publisher+'</li>';
+			html += '<li>Rating: '+item.rating+' ('+item.ratings_count+' ratings)</li>';
+			html += '<li>Added: '+item.added+'</li>';
+			html += '</ul>';
 			html += '</div>';
 			html += '</div>';
 			html += '</div>';
@@ -22,11 +33,17 @@ $(document).ready(function() {
 	}
 	$.getJSON('books-data.json', function(data) {
 		$('#pagination-container').pagination({
-			dataSource: data.items,
+			dataSource: data,
 			totalNumberLocator: function(response) {
-				return data.items.length;
+				return data.length;
 			},
+			locator: "",
+			showGoInput: true,
+			showGoButton: true,
+			formatGoInput: 'go to <%= input %> st/rd/th',
 			pageSize: 4,
+			showPageNumbers: false,
+			showNavigator: true,
 			callback: function(data, pagination) {
 				dataContainer.html(simpleTemplating(data));
 			},
